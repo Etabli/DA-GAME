47a6f4c8434e47dd2f09cd232865dbee7aa5fd1c
Dominik Toedling <dominiktoedling95@gmail.com>
2017-06-04 13:58:50 +0200

    Added AttributePool to ItemBase (not used in generation yet)

--- Assets/Scripts/Equipment/ItemBase.cs
+++ Assets/Scripts/Equipment/ItemBase.cs
@@ -33,14 +33,21 @@ public abstract class ItemBase
     public readonly AttributeType[] BaseAttributes;
     [DataMember]
     public readonly AttributeType[] GuaranteedAttributes;
+    [DataMember]
+    public readonly AttributePool PossibleAttributes;
 
-    public ItemBase(ItemBaseType baseType, ItemClass itemClass, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes)
+    public ItemBase(ItemBaseType baseType, ItemClass itemClass, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AttributePool attributePool)
     {
         BaseType = baseType;
         Class = itemClass;
         BaseAttributes = baseAttributes.Clone() as AttributeType[];
         GuaranteedAttributes = guaranteedAttributes.Clone() as AttributeType[];
+        PossibleAttributes = attributePool;
     }
+
+    public ItemBase(ItemBaseType baseType, ItemClass itemClass, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AttributePoolPreset attributePoolPreset)
+        : this(baseType, itemClass, baseAttributes, guaranteedAttributes, AttributePool.GetPool(attributePoolPreset))
+    { }
 
     /// <summary>
     /// Generates a new item of this base type
@@ -75,13 +82,18 @@ public abstract class ItemBase
             tierLottery.Enter(item.Tier - 3, 1);
 
         int quality = item.Quality;
-        Debug.Log(string.Format("quality = {0}", quality));
+        //Debug.Log(string.Format("quality = {0}", quality));
 
+        // Guaranteed attributes don't care about possible pool
         foreach (AttributeType attributeType in GuaranteedAttributes)
         {
+            int tier = tierLottery.GetWinner();
+            item.Attributes.Add(attributeType, AttributeInfo.GetAttributeInfo(attributeType).GenerateAttribute(tier));
+            quality -= tier;
             GenerateAttributeForItem(tierLottery, ref quality, item, attributeType);
         }
 
+        // Fill up the rest of the attributes
         while (quality > 0)
         {
             Debug.Log("Generating random attribute");
@@ -99,13 +111,12 @@ public abstract class ItemBase
         quality -= tier;
 
         Attribute attribute;
-        int i = 0;
         do
         {
             attribute = AttributeInfo.GetAttributeInfo(attributeType).GenerateAttribute(tier);
             Debug.Log(attribute.Type);
 
-        } while (item.Attributes.ContainsKey(attribute.Type) && i < 10);
+        } while (item.Attributes.ContainsKey(attribute.Type));
 
         item.Attributes.Add(attribute.Type, attribute);
     }
@@ -117,10 +128,15 @@ public class WeaponBase : ItemBase
     [DataMember]
     public readonly AmmoClass[] AllowedAmmoTypes;
 
-    public WeaponBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AmmoClass[] allowedAmmoTypes) : base(baseType, ItemClass.Weapon, baseAttributes, guaranteedAttributes)
+    public WeaponBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AmmoClass[] allowedAmmoTypes, AttributePool attributePool)
+        : base(baseType, ItemClass.Weapon, baseAttributes, guaranteedAttributes, attributePool)
     {
         AllowedAmmoTypes = allowedAmmoTypes.Clone() as AmmoClass[];
     }
+
+    public WeaponBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AmmoClass[] allowedAmmoTypes, AttributePoolPreset attributePoolPreset)
+        : this(baseType, baseAttributes, guaranteedAttributes, allowedAmmoTypes, AttributePool.GetPool(attributePoolPreset))
+    { }
 
     public override Item GenerateItem(int tier, int quality)
     {
@@ -136,10 +152,15 @@ public class ArmorBase : ItemBase
     [DataMember]
     public readonly Slot[] Slots;
 
-    public ArmorBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, Slot[] slots) : base(baseType, ItemClass.Armor, baseAttributes, guaranteedAttributes)
+    public ArmorBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, Slot[] slots, AttributePool attributePool)
+        : base(baseType, ItemClass.Armor, baseAttributes, guaranteedAttributes, attributePool)
     {
         Slots = slots.Clone() as Slot[];
     }
+
+    public ArmorBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, Slot[] slots, AttributePoolPreset attributePoolPreset)
+        : base(baseType, ItemClass.Armor, baseAttributes, guaranteedAttributes, AttributePool.GetPool(attributePoolPreset))
+    { }
 
     public override Item GenerateItem(int tier, int quality)
     {
@@ -154,10 +175,15 @@ public class AmmoBase : ItemBase
     [DataMember]
     public readonly AmmoClass AmmoClass;
 
-    public AmmoBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AmmoClass ammoClass) : base(baseType, ItemClass.Ammo, baseAttributes, guaranteedAttributes)
+    public AmmoBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AmmoClass ammoClass, AttributePool attributePool)
+        : base(baseType, ItemClass.Ammo, baseAttributes, guaranteedAttributes, attributePool)
     {
         AmmoClass = ammoClass;
     }
+
+    public AmmoBase(ItemBaseType baseType, AttributeType[] baseAttributes, AttributeType[] guaranteedAttributes, AmmoClass ammoClass, AttributePoolPreset attributePoolPreset)
+        : base(baseType, ItemClass.Ammo, baseAttributes, guaranteedAttributes, AttributePool.GetPool(attributePoolPreset))
+    { }
 
     public override Item GenerateItem(int tier, int quality)
     {
